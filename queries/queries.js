a14945eb41facae4691052d33d05fc1786759b38
iamansingh0 <amankumar7355197337@gmail.com>
2024-01-24 18:37:18 +0530

    queries

--- queries/queries.js
+++ queries/queries.js
@@ -1,5 +1,4 @@
 // How many users are active?
-
 [
     {
       $match: {
@@ -9,4 +8,36 @@
     {
       $count: "Active Users"
     }
+]
+
+// average age of all users
+[
+  {
+    $group: {
+      _id: null,
+      averageAge: {
+        $avg: "$age"
+      }
+    }
+  }
+]
+
+// top 5 most common favorite fruits among users
+[
+  {
+    $group: {
+      _id: "$favoriteFruit",
+      count: {
+        $sum: 1
+      }
+    }
+  },
+  {
+    $sort: {
+      count: -1
+    }
+  },
+  {
+    $limit: 5
+  }
 ]
--- queries/questions.md
+++ queries/questions.md
@@ -1,1 +1,3 @@
 1. How many users are active?
+2. What is the average age of all users?
+3. List the top 5 most common favorite fruits among users
